32efe05c82cfe164ac3d3a5af7353ec430ca17d9
ItaiAxelrad <itaiaxelrad@gmail.com>
2020-08-08 10:08:42 -0600

    mobile UI

--- src/scripts/modules/mode.js
+++ src/scripts/modules/mode.js
@@ -1,22 +1,37 @@
 const body = document.querySelector('body')
 const toggleBtn = document.querySelector('#schemeBtn')
 const useLink = document.querySelector('#schemeBtn use')
+
+// set initial preferences
+if (window.matchMedia('(prefers-color-scheme: dark)').matches || localStorage.getItem('mode') == 'dark' ) {
+        body.classList.add('scheme-dark')
+        body.classList.remove('scheme-light')
+        useLink.setAttribute("xlink:href", "images/defs.svg#light")
+        localStorage.setItem('mode', 'dark')
+    } else if (window.matchMedia('(prefers-color-scheme: light)').matches || localStorage.getItem('mode') == 'light') {
+        body.classList.add('scheme-light')
+        body.classList.remove('scheme-dark')
+        useLink.setAttribute("xlink:href", "images/defs.svg#dark")
+        localStorage.setItem('mode', 'light')
+    }
+
 // toggle button event listener
 toggleBtn.addEventListener('click', () => {
     // check the user's preferred color scheme
-    if (window.matchMedia('(prefers-color-scheme: dark)').matches || localStorage.getItem('mode') == 'dark' ) {
+    if (localStorage.getItem('mode') == 'dark') {
         body.classList.remove('scheme-dark')
         body.classList.add('scheme-light')
         useLink.setAttribute("xlink:href", "images/defs.svg#dark")
         localStorage.setItem('mode', 'light')
-    } else if (window.matchMedia('(prefers-color-scheme: light)').matches || localStorage.getItem('mode') == 'light') {
+    } else if (localStorage.getItem('mode') == 'light') {
         body.classList.remove('scheme-light')
         body.classList.add('scheme-dark')
         useLink.setAttribute("xlink:href", "images/defs.svg#light")
         localStorage.setItem('mode', 'dark')
     } 
 })
-
+// || localStorage.getItem('mode') == 'dark' 
+// || localStorage.getItem('mode') == 'light'
 //export module
 export {
     body,
